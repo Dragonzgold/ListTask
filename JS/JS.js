82887e58362dc04ac7d4befa741d02adcf1a1143
unknown <ramirezjesus55@hotmail.com>
2024-11-26 20:48:04 +0100

    change and update list

--- JS/JS.js
+++ JS/JS.js
@@ -1,12 +1,28 @@
 const buttonEvent = document.querySelector('#buttonSave');
+const taskList = document.querySelector('tbody');
 
-buttonEvent.addEventListener('click', function(){
+buttonEvent.addEventListener('click', function () {
 
-    tarea = document.querySelector('#textInput').value;
+  tarea = document.querySelector('#textInput').value;
 
-    if(tarea.trim() == ''){
-        alert("usted introdujo un campo vacio. Por favor rellenelo con una tarea")
-        return;
-    }
-    console.log("prueba2")
+  if (tarea.trim() == '') {
+    alert("usted introdujo un campo vacio. Por favor rellenelo con una tarea")
+    return;
+  }
+
+  const newRow = document.createElement('tr');
+
+  const taskCell = document.createElement('td');
+  taskCell.textContent = tarea;
+
+  const checkboxCell = document.createElement('td');
+  // const checkbox = document.createElement('input');
+  // checkbox.type = 'checkbox';
+  // checkboxCell.appendChild(checkbox);
+
+  newRow.appendChild(taskCell);
+  newRow.appendChild(checkboxCell);
+
+  taskList.appendChild(newRow);
+
 })
